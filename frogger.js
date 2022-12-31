47f1c1823ee448656570d282bd26f9071c4156aa
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2022-12-30 22:57:48 -0600

    javascript file

---
+++ frogger.js
@@ -0,0 +1,10 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const startPauseButton = document.querySelector('start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+
+function moveFrog(){
+
+}
+
+document.addEventListener(keyup, moveFrog)
